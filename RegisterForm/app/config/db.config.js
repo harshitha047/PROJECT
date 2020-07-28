d7fccd8359c106dd2dce1813b757776cb6d0eb05
harshitha047 <saimpuharshitha47@gmail.com>
2020-07-28 17:05:58 +0530

    PROJECT

--- RegisterForm/app/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST : "localhost",
-    USER : "root",
-    PASSWORD : "Gitam@123",
-    DB : "project"
-}
--- RegisterForm/app/routes/register.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app =>{
-    const registers = require('../controllers/register.controller.js');
-
-    //create a new register
-    app.post ("/registerform",registers.create);
-    //single user
-    app.get("/registerform",registers.findOne);
-}
